0303c923ec808e301297234026b2cda64b08ebbe
HONGWON <w___on@naver.com>
2024-12-08 21:59:47 +0900

    chore: 프로젝트 세팅

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,65 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
       },
+      colors: {
+        "white": "#ffffff",
+        "background": "#f9f9f9",
+        "primary": "#a2c3e7",
+        "secondary": "#578fcc",
+        "gray": {
+          "01": "#ababab",
+          "02": "#636363",
+          "03": "#202020",
+        },
+      },
+      fontSize: {
+        "3xl": [
+          "1.75rem", // 28px
+          {
+            lineHeight: "1.8125rem",
+          }
+        ],
+        "2xl": [
+          "1.5rem", // 24px
+          {
+            lineHeight: "1.5rem",
+          }
+        ],
+        "xl": [
+          "1.25rem", // 20px
+          {
+            lineHeight: "1.25rem",
+          }
+        ],
+        lg: [
+          "1.125rem", // 18px
+          {
+            lineHeight: "1.125rem",
+          }
+        ],
+        base: [
+          "1rem", // 16px
+          {
+            lineHeight: "1rem",
+          }
+        ],
+        sm: [
+          "0.875rem", // 14px
+          {
+            lineHeight: "0.875rem",
+          }
+        ],
+      },
+      fontFamily: {
+        sans: ["var(--font-pretendard)", "sans-serif"],
+      } 
     },
   },
   plugins: [],
