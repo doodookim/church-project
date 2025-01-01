f1b5caca0cc8223a3de81ab3a02b90d405249020
LeeDongTak <sunny5796@naver.com>
2025-01-01 23:06:08 +0900

    fix: route 구조 변경

---
+++ app/(user)/my-page/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "마이 페이지 | 생명의 빛 교회",
+  description: "마이 페이지 페이지 입니다.",
+};
+
+export default function MyPagePage() {
+  return <div>마이 페이지</div>;
+}
