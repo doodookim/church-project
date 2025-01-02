6d215e2c2086cf768668d35c91b9eb62402d00c2
LeeDongTak <sunny5796@naver.com>
2025-01-02 23:15:52 +0900

    feat : 각 메뉴의 data 정의

--- app/module/components/layout/constant.ts
+++ app/module/components/layout/constant.ts
@@ -21,4 +21,172 @@ const HEADER_MENU_LIST = [
   },
 ];
 
-export { HEADER_MENU_LIST };
+// 교회 안내
+const NOTICE_SIDEBAR_LIST = [
+  {
+    id: 0,
+    name: "교회 안내",
+    url: "/notice",
+    isClick: false,
+    subMenuList: [
+      {
+        id: 0,
+        name: "교회 소개",
+        url: "",
+        isClick: false,
+      },
+      {
+        id: 1,
+        name: "담임목사 소개",
+        url: "",
+        isClick: false,
+      },
+      {
+        id: 2,
+        name: "표어",
+        url: "",
+        isClick: false,
+      },
+      {
+        id: 3,
+        name: "예배 안내",
+        url: "",
+        isClick: false,
+      },
+      {
+        id: 4,
+        name: "오시는 길",
+        url: "",
+        isClick: false,
+      },
+    ],
+  },
+  {
+    id: 1,
+    name: "다음 세대",
+    url: "/notice/next-generation",
+    isClick: false,
+    subMenuList: [
+      {
+        id: 0,
+        name: "유치부",
+        url: "",
+        isClick: false,
+      },
+      {
+        id: 1,
+        name: "아동부",
+        url: "",
+        isClick: false,
+      },
+      {
+        id: 2,
+        name: "청소년부",
+        url: "",
+        isClick: false,
+      },
+      {
+        id: 3,
+        name: "청년부",
+        url: "",
+        isClick: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "교회 조직",
+    url: "/notice/organization",
+    isClick: false,
+  },
+  {
+    id: 3,
+    name: "시설 안내",
+    url: "/notice/facility",
+    isClick: false,
+  },
+];
+
+// 교회 소식
+const NEWS_SIDEBAR_LIST = [
+  {
+    id: 0,
+    name: "교회 소식",
+    url: "/news",
+    isClick: false,
+  },
+
+  {
+    id: 1,
+    name: "주보",
+    url: "/news/weekly",
+    isClick: false,
+  },
+  {
+    id: 2,
+    name: "갤러리",
+    url: "/news/gallery",
+    isClick: false,
+  },
+];
+
+// 예배 및 집회
+const WORSHIP_SIDEBAR_LIST = [
+  {
+    id: 0,
+    name: "예배 및 집회",
+    url: "/worship",
+    isClick: false,
+    subMenuList: [
+      {
+        id: 0,
+        name: "주일 예배",
+        url: "",
+        isClick: false,
+      },
+      {
+        id: 1,
+        name: "주일 특별 예배",
+        url: "",
+        isClick: false,
+      },
+      {
+        id: 2,
+        name: "찬양 예배",
+        url: "",
+        isClick: false,
+      },
+    ],
+  },
+];
+
+const MISSION_SIDEBAR_LIST = [
+  {
+    id: 0,
+    name: "선교",
+    url: "#",
+    isClick: true,
+    subMenuList: [
+      {
+        id: 0,
+        name: "선교지 안내",
+        url: "/mission",
+        isClick: false,
+      },
+      {
+        id: 1,
+        name: "선교 갤러리",
+        url: "/mission/gallery",
+        isClick: false,
+      },
+    ],
+  },
+];
+
+export {
+  HEADER_MENU_LIST,
+  NOTICE_SIDEBAR_LIST,
+  NEWS_SIDEBAR_LIST,
+  WORSHIP_SIDEBAR_LIST,
+  MISSION_SIDEBAR_LIST,
+};
