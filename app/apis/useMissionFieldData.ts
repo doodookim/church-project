86ad03820984959e67390d8550be0a19f5131501
ohseoungyeon <alstmd9902@naver.com>
2025-01-04 20:44:27 +0900

    style/오시는길 지도 이미지첨부

--- app/apis/useMissionFieldData.ts
+++ app/apis/useMissionFieldData.ts
@@ -2,17 +2,19 @@ import { IchurchNotice, IimageData } from "@/utils/types/churchData";
 import { useQuery } from "@tanstack/react-query";
 
 const getNoticeData = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/notice-board/mission-field/`);
-    const data = await res.json();
-    return data;
-  };
-  
-  const useFetchNotice = () => {
-    const { data, isLoading } = useQuery<IimageData[]>({
-      queryKey: ["churchmission-field"],
-      queryFn: getNoticeData,
-    });
-  
-    return { data, isLoading };
-  };
-  export default useFetchNotice;
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/notice-board/mission-field/`
+  );
+  const data = await res.json();
+  return data;
+};
+
+const useFetchNotice = () => {
+  const { data, isLoading } = useQuery<IimageData[]>({
+    queryKey: ["churchmission-field"],
+    queryFn: getNoticeData,
+  });
+
+  return { data, isLoading };
+};
+export default useFetchNotice;
--- utils/types/churchData.ts
+++ utils/types/churchData.ts
@@ -1,14 +1,27 @@
 export interface IimageData {
-    id: number;
-    image_files: string;
-  }
-  
-  export interface IchurchNotice {
-    church_info_content: string;
-    church_info_img: IimageData;
-    pastor_info_content: string;
-    pastor_img: IimageData;
-    slogan_img: IimageData;
-    worship_time_img: IimageData;
-    location: string;
-  }
+  id: number;
+  image_files: string;
+}
+
+export interface IchurchNotice {
+  church_info_content: string;
+  church_info_img: IimageData;
+  pastor_info_content: string;
+  pastor_img: IimageData;
+  slogan_img: IimageData;
+  worship_time_img: IimageData;
+  location: string;
+}
+
+interface IGalleyAll {
+  id: number;
+  title: string;
+  date: string;
+  mission_gallery_thumb_img: IimageData;
+}
+export interface IMissionGalleryAll {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: IGalleyAll[];
+}
