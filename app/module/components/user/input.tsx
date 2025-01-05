8f4dc265a2f5551f4c3e12f6eb5a70b3e410caf5
LeeDongTak <sunny5796@naver.com>
2025-01-05 17:11:42 +0900

    fix: Input컴포넌트 props추가

--- app/module/components/user/input.tsx
+++ app/module/components/user/input.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import React from "react";
 
 interface IInput {
@@ -5,16 +6,26 @@ interface IInput {
   name: string;
   id: string;
   placeholder?: string;
+  classNamePlus?: string;
 }
 
-export default function Input({ type, name, id, placeholder }: IInput) {
+export default function Input({
+  type,
+  name,
+  id,
+  placeholder,
+  classNamePlus,
+}: IInput) {
   return (
     <input
       type={type}
       name={name}
       id={id}
       placeholder={placeholder}
-      className="text-[16px] font-medium py-[13px] border-b border-solid border-[#ABABAB] focus:outline-none"
+      className={clsx(
+        "text-[16px] font-medium py-[13px] border-b border-solid border-[#ABABAB] focus:outline-none",
+        classNamePlus
+      )}
     />
   );
 }
