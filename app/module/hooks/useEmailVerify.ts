54883748fb72a3441f9645452787f399ee944d10
LeeDongTak <sunny5796@naver.com>
2025-01-07 22:18:40 +0900

    fix: 인증번호 확인 로직 주석처리 추후 기획변경을 위해 주석처리

--- app/module/hooks/useEmailVerify.ts
+++ app/module/hooks/useEmailVerify.ts
@@ -7,6 +7,7 @@ import {
 import { TSignUp } from "../types/sign-up-types";
 import { UseMutateFunction } from "@tanstack/react-query";
 import { ISendEmailRequest } from "../types/send-email-types";
+// import { IVerifyCodeRequest } from "../types/verify-code-types";
 import { IResponseType } from "../types/response-types";
 
 interface IUseEmailVerify {
@@ -14,18 +15,29 @@ interface IUseEmailVerify {
   getValues: UseFormGetValues<TSignUp>;
   trigger: UseFormTrigger<TSignUp>;
   sendEmailData: IResponseType;
+  // verifyCodeData: IResponseType;
   sendEmailMutate: UseMutateFunction<
     unknown,
     Error,
     ISendEmailRequest,
     unknown
   >;
+  // verifyCoedMutate: UseMutateFunction<
+  //   unknown,
+  //   Error,
+  //   IVerifyCodeRequest,
+  //   unknown
+  // >;
+}
+
 const useEmailVerify = ({
   getFieldState,
   getValues,
   trigger,
   sendEmailData,
+  // verifyCodeData,
   sendEmailMutate,
+}: // verifyCoedMutate,
 IUseEmailVerify) => {
   // send email
   const [isSendEmailDone, setIsSendEmailDone] = useState(false);
@@ -60,10 +72,44 @@ IUseEmailVerify) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isSendEmailDone, sendEmailData]);
 
+  //////////////////////////////////////////////
+  //////////////////////////////////////////////
+  //////////////////////////////////////////////
+  //////////////////////////////////////////////
+
+  // verify code 백앤드 로직에 따른 주석 추후 기획변경을 위해 주석처리
+  // const [isVerifyCodeDone, setIsVerifyCodeDone] = useState(false);
+  // const [verifyCodeMessage, setVerifyCodeMessage] = useState("");
+  // const verifyCodeState = getFieldState("verifyCode");
+  // const code = getValues("verifyCode");
+
+  // const clickVerifyCodeHandler = () => {
+  //   if (!emailState.isDirty || emailState.invalid) {
+  //     trigger(["email"]);
+  //     return;
+  //   }
+  //   if (!verifyCodeState.isDirty || verifyCodeState.invalid) {
+  //     trigger(["verifyCode"]);
+  //     return;
+  //   }
+  //   verifyCoedMutate({ email, code, setIsVerifyCodeDone });
+  // };
+
+  // useEffect(() => {
+  //   if (!verifyCodeState.isDirty || verifyCodeState.invalid) return;
+  //   if (isVerifyCodeDone && verifyCodeData) {
+  //     setVerifyCodeMessage(verifyCodeData?.message);
+  //   } else if (!isVerifyCodeDone && !verifyCodeData) {
+  //     setVerifyCodeMessage("");
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [isVerifyCodeDone, verifyCodeData]);
+
   return {
     isSendEmailDone,
     sendEmailMessage,
     clickSendEmailHandler,
+    // clickVerifyCodeHandler,
   };
 };
 
