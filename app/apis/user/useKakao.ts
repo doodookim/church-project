20aa781eaf7b32c22e7c4bcc85d6810ae11db7ce
LeeDongTak <sunny5796@naver.com>
2025-01-15 19:52:52 +0900

    feat: 카카오 로그인 구현

---
+++ app/apis/user/useKakao.ts
@@ -0,0 +1,28 @@
+import { IKakaoRequest } from "@/app/module/types/kakao-request";
+import { useMutation } from "@tanstack/react-query";
+import { signIn } from "next-auth/react";
+
+const kakaoRequest = async ({ code, push }: IKakaoRequest) => {
+  const res = await signIn("kakaoId", {
+    code,
+    redirect: false,
+  });
+
+  if (!res?.ok) {
+    throw new Error(res?.error as string);
+  }
+
+  push("/");
+  return res;
+};
+
+const useKakao = () => {
+  const { data, mutate, isPending, isError, error } = useMutation({
+    mutationKey: ["kakao"],
+    mutationFn: kakaoRequest,
+  });
+
+  return { data, mutate, isPending, isError, error };
+};
+
+export { useKakao };
