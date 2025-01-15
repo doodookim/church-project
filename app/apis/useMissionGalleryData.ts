be6c3850f35835273a2ab07b00fe05dc8e9715e5
LeeDongTak <sunny5796@naver.com>
2025-01-15 21:30:02 +0900

    fix: 이름 변경

--- app/apis/useMissionGalleryData.ts
+++ app/apis/useMissionGalleryData.ts
@@ -1,18 +1,21 @@
 import { IchurchNotice, IMissionGalleryAll } from "@/utils/types/churchData";
 import { useQuery } from "@tanstack/react-query";
 
-const getNoticeData = async () => {
+const getMissionGallery = async () => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/notice-board/mission-gallery/`
   );
   const data = await res.json();
+  if (!res.ok) {
+    throw new Error(data.message);
+  }
   return data;
 };
 
 const useFetchMissionGallery = () => {
   const { data, isLoading, isError, error } = useQuery<IMissionGalleryAll>({
     queryKey: ["churchmissiongallery"],
-    queryFn: getNoticeData,
+    queryFn: getMissionGallery,
   });
 
   return { data, isLoading, isError, error };
