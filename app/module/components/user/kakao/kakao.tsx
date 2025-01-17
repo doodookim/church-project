303339310285ba4ced55b54129c35191868e753a
LeeDongTak <sunny5796@naver.com>
2025-01-17 15:25:57 +0900

    fix: 로딩스피너 추가

--- app/module/components/user/kakao/kakao.tsx
+++ app/module/components/user/kakao/kakao.tsx
@@ -25,42 +25,39 @@ export default function Kakao() {
 
   if (isError) {
     return (
-      <div className="fixed top-0 left-0 w-full h-[100vh] z-50">
-        <BoxLayout
-          width={650}
-          classNamePlus="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"
-        >
-          <div className="mt-[108px] mb-[113px] flex flex-col justify-center items-center gap-[40px]">
-            <TitleLayout
-              title="로그인에 문제가 생겼습니다"
-              classNamePlus="flex justify-center"
-            />
-            <div className="flex gap-[18px]">
-              <UserButton>
-                <Link
-                  href={`https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}`}
-                  className="flex justify-center items-center w-full h-full"
-                >
-                  다시 로그인
-                </Link>
-              </UserButton>
-              <UserButton>
-                <Link
-                  href={"/"}
-                  className="flex justify-center items-center w-full h-full"
-                >
-                  메인으로
-                </Link>
-              </UserButton>
-            </div>
+      <BoxLayout width={650}>
+        <div className="mt-[108px] mb-[113px] flex flex-col justify-center items-center gap-[40px]">
+          <TitleLayout
+            title="로그인에 문제가 생겼습니다"
+            classNamePlus="flex justify-center"
+          />
+          <div className="flex gap-[18px]">
+            <UserButton>
+              <Link
+                href={`https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}`}
+                className="flex justify-center items-center w-full h-full"
+              >
+                다시 로그인
+              </Link>
+            </UserButton>
+            <UserButton>
+              <Link
+                href={"/"}
+                className="flex justify-center items-center w-full h-full"
+              >
+                메인으로
+              </Link>
+            </UserButton>
           </div>
-        </BoxLayout>
-      </div>
+        </div>
+      </BoxLayout>
     );
   }
   return (
-    <div className="mt-[108px] mb-[113px] flex flex-col justify-center items-center gap-[40px]">
-      <LoadingSpinner boxSize={3.5} ballSize={0.4} color="#578fcc" />
-    </div>
+    <BoxLayout width={650}>
+      <div className="mt-[108px] mb-[113px] flex flex-col justify-center items-center gap-[40px]">
+        <LoadingSpinner boxSize={3.5} ballSize={0.4} color="#578fcc" />
+      </div>
+    </BoxLayout>
   );
 }
