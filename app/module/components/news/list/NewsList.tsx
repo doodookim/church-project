c6e06e1bdaba57b61dd750c11769eb2139991d99
ohseoungyeon <alstmd9902@naver.com>
2025-01-18 22:14:41 +0900

    style/교회소식,갤러리 및 페이지네이션 퍼블리싱 완료

--- app/module/components/news/list/NewsList.tsx
+++ app/module/components/news/list/NewsList.tsx
@@ -14,7 +14,6 @@ export default function NewsList({ data, isLoading }: INewsListProps) {
   }
   if (isLoading) return <div>로딩 중입니다</div>;
   return (
-<<<<<<< HEAD
     <div>
       <h2 className="text-[#578FCC] text-2xl font-bold">교회소식</h2>
       <div className="border-y-2 mt-[40px] border-solid border-[#578FCC]">
@@ -42,30 +41,6 @@ export default function NewsList({ data, isLoading }: INewsListProps) {
           </div>
         ))}
       </div>
-=======
-    <div className="border-y-2 mt-[40px]">
-      {data?.results.map((news) => (
-        <div key={news.id}>
-          <Link
-            href={`/news/${news.id}`}
-            className="flex justify-between items-center text-center border-b boder-gray-200 pb-4 cursor-pointer hover:bg-gray-50 p-4 rounded"
-          >
-            <div className="flex items-center gap-[30px] ">
-              {news.notification ? (
-                <span className="border-[2px] border-[#578fcc] text-[#578fcc]  rounded-[5px] px-3 py-1 text-[14px] font-bold">
-                  공지
-                </span>
-              ) : (
-                <div>{news.id}</div>
-              )}
-              <h2 className="text-lg font-medium">{news.title}</h2>
-            </div>
-
-            <p className="flex text-gray-600 text-sm ">{news.date}</p>
-          </Link>
-        </div>
-      ))}
->>>>>>> a37147b763a15e83d7ff116b5eac55635f7b881c
     </div>
   );
 }
